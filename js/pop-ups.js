e01cff605c234bc7f660ef9f739bd0278b739258
Daniel Chima <37340699+dchima@users.noreply.github.com>
2019-05-03 00:07:31 +0100

    added new changes
    adjusted the academics page to include div appearance on hover and not click of logos.
    transition property not as smooth as i would like but site is stable

--- js/pop-ups.js
+++ js/pop-ups.js
@@ -1,31 +1,88 @@
-$(document).ready(function(){
-    $('.bhc').click(function() {
-      $('.menu').slideToggle("slow");$('.menu2').fadeOut("slow");
-      $('.menu3').fadeOut("slow");$('.menu4').fadeOut("slow");
-    });
-    $('.essex').click(function() {
-      $('.menu').fadeOut("slow");$('.menu2').slideToggle("slow");
-      $('.menu3').fadeOut("slow");$('.menu4').fadeOut("slow");
-    });
-    $('.nottingham').click(function() {
-      $('.menu').fadeOut("slow");$('.menu2').fadeOut("slow");
-      $('.menu3').slideToggle("slow");$('.menu4').fadeOut("slow");
-    });
+$(function(){
 
-    $('.leeds').click(function() {
-      $('.menu').fadeOut("slow");$('.menu2').fadeOut("slow");
-      $('.menu3').fadeOut("slow");$('.menu4').slideToggle("slow");
+    $('.leeds').hover(function() {
+      $('.menu').css("width", "1050px");
+    }, function (){
+      $('.menu').css("width", "0px");
     });
+});
 
-    $('.about').click(function() {
-      $('.about-page').fadeIn("slow");
-      $('.wrapper').fadeOut(0);
-    });
-
-    $('.exit').click(function() {
-      $('.wrapper').fadeIn("slow");
-      $('.about-page').hide(0);
-
-    });
+$(function(){
 
 });
+
+$(function(){
+  $('.nottingham').hover(function() {
+    $('.menu2').css("width", "1050px");
+  }, function (){
+    $('.menu2').css("width", "0px");
+  });
+});
+
+$(function(){
+  $('.essex').hover(function() {
+    $('.menu3').css("width", "1050px");
+  }, function (){
+    $('.menu3').css("width", "0px");
+  });
+});
+
+$(function(){
+  $('.bhc').hover(function() {
+    $('.menu4').css("width", "1050px");
+  }, function (){
+    $('.menu4').css("width", "0px");
+  });
+});
+
+$(function(){
+  $('.about').click(function() {
+    $('.about-page').fadeIn("slow");
+    $('.wrapper').fadeOut(0);
+  });
+});
+
+$(function(){
+  $('.exit').click(function() {
+    $('.wrapper').fadeIn("slow");
+    $('.about-page').hide(0);
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//old code for click of logos
+/*$('.bhc').click(function() {
+  $('.menu').slideToggle("slow");$('.menu2').fadeOut("slow");
+  $('.menu3').fadeOut("slow");$('.menu4').fadeOut("slow");
+
+});
+$('.essex').click(function() {
+  $('.menu').fadeOut("slow");$('.menu2').slideToggle("slow");
+  $('.menu3').fadeOut("slow");$('.menu4').fadeOut("slow");
+});
+$('.nottingham').click(function() {
+  $('.menu').fadeOut("slow");$('.menu2').fadeOut("slow");
+  $('.menu3').slideToggle("slow");$('.menu4').fadeOut("slow");
+});
+
+$('.leeds').click(function() {
+  $('.menu').fadeOut("slow");$('.menu2').fadeOut("slow");
+  $('.menu3').fadeOut("slow");$('.menu4').slideToggle("slow");
+});*/
