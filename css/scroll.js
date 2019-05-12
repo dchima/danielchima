5950ae77aa02bf8e1e2e17dc9830a08c4a7a91f6
Daniel Chima <37340699+dchima@users.noreply.github.com>
2019-05-12 20:59:07 +0100

    redesigned sight map
    small changes to font and color, removed div navigation buttons

---
+++ css/scroll.js
@@ -0,0 +1,58 @@
+
+$(function() {
+   $('.scroll-down1').click (function() {
+     $('html, body').animate({scrollTop: $('div.bg2').offset().top }, 'slow');
+     return false;
+   });
+});
+
+$(function() {
+   $('.scroll-down2').click (function() {
+     $('html, body').animate({scrollTop: $('div.bg3').offset().top }, 'slow');
+     return false;
+   });
+});
+
+$(function() {
+   $('.scroll-down3').click (function() {
+     $('html, body').animate({scrollTop: $('div.bg4').offset().top }, 'slow');
+     return false;
+   });
+});
+
+$(function() {
+   $('.scroll-down4').click (function() {
+     $('html, body').animate({scrollTop: $('div.bg5').offset().top }, 'slow');
+     return false;
+   });
+});
+
+
+
+$(function() {
+   $('.scroll-up1').click (function() {
+     $('html, body').animate({scrollTop: $('div.bg1').offset().top }, 'slow');
+     return false;
+   });
+});
+
+$(function() {
+   $('.scroll-up2').click (function() {
+     $('html, body').animate({scrollTop: $('div.bg2').offset().top }, 'slow');
+     return false;
+   });
+});
+
+$(function() {
+   $('.scroll-up3').click (function() {
+     $('html, body').animate({scrollTop: $('div.bg3').offset().top }, 'slow');
+     return false;
+   });
+ });
+
+$(function() {
+  $('.scroll-up4').click (function() {
+    $('html, body').animate({scrollTop: $('div.bg4').offset().top }, 'slow');
+    return false;
+  });
+});
---
+++ js/scroll-fade.js
@@ -0,0 +1,16 @@
+
+/*var scrollFade = function(){
+  var div = $(".fader:not(.animated)");
+  var b = $(window).scrollTop() + $(window).height();
+
+  div.each(function (x){
+    var a = $(this).offset().top + $(this).height();
+
+    if(a < b){
+      $this.addClass(".animated").addClass(".fadeInUp");
+    }
+  });
+};
+
+$(scrollFade);
+$(window).scroll(scrollFade);*/
